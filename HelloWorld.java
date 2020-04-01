9c557a21e30ff5f9e16cbdc8706e45abace2f0fa
ss-waree <wsethapun20@deerfield.edu>
2020-04-01 16:20:17 +0700

    April Fool's Day

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld extends ConsoleProgram{
 	public void run() {
 		println("Hello World");
 		println("Send help, I've been stuck here for 6 days.");
+		println("Only 8 days left till I am freeeee!");
 	}
 }
