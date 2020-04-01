519767786e7a5807757fef1da14cf08f93fb3d46
ss-waree <wsethapun20@deerfield.edu>
2020-04-01 16:08:32 +0700

    Signed-off-by: ss-waree <wsethapun20@deerfield.edu>

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ import acm.program.*;
 public class HelloWorld extends ConsoleProgram{
 	public void run() {
 		println("Hello World");
+		println("Send help, I've been stuck here for 6 days.");
 	}
 }
